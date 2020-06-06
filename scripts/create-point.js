84e8385d4be5d63d6c2be7422de356ca298b5a18
Antonio Lima <alimamg4@gmail.com>
2020-06-06 16:39:16 -0300

    API IBGE Estados

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,8 +2,39 @@ function populateUFs() {
 const ufSelect = document.querySelector("select[name=uf]")
 
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/distritos")
-.then( (res) => { return res.json() })
+.then( res => res.json() )
 .then( states => {
 
-ufSelect.innerHTML = `<option value="1">Valor</option>
+for ( const state of states ) {
+ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+}
+})
+}
+
+populateUFs()
+
+function getCities(event) {
+
+const citySelect = document.querySelector("select[name=city]")
+
+const ufValue = event.target.value
+
+const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+fetch(url)
+.then( res => res.json() )
+.then( cities => {
+
+for ( const city of cities ) {
+citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+}
+
+citySelect.disabled = false
+
+})
+}
+
+document
+.querySelector("select[name=uf]")
+.addEventListener("change", getCities)
 
