86a72f484eb8fb19a39ad12da8566950f4130059
Antonio Lima <alimamg4@gmail.com>
2020-06-06 22:16:09 -0300

    Update Cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,36 +1,42 @@
-function populateUFs() {
+
+//Função Estados//
+function populateUFs () {
+
 const ufSelect = document.querySelector("select[name=uf]")
-
-fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/distritos")
-.then( res => res.json() )
+fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome")
+then( res => res.json())
 .then( states => {
-
 for ( const state of states ) {
 ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
 }
+
 })
+
 }
 
 populateUFs()
 
-function getCities(event) {
-
-const citySelect = document.querySelector("select[name=city]")
-
+//Função Cidades//
+function getCities(event){
+const citySelect = document.querySelector("[name=city]")
+const stateInput = document.querySelector("[name=state]")
 const ufValue = event.target.value
+const indexOfSelectedState = event.target.selectedIndex
+stateInput.value = event.target.options[indexOfSelectedState].text
 
-const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
+const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios?orderBy=nome`
+    
+citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+citySelect.disabled = true
+    
 fetch(url)
-.then( res => res.json() )
+.then( res => res.json())
 .then( cities => {
-
 for ( const city of cities ) {
-citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
 }
 
 citySelect.disabled = false
-
 })
 }
 
